2b3656c5eefe2a42e5711e2da821fb669f7aaea3
Sohva <33163737+Sohva@users.noreply.github.com>
2017-10-28 22:25:03 +0100

    merge

--- monstercreator.py
+++ monstercreator.py
@@ -6,13 +6,14 @@ import monster
 
 def createMonsters(case):
     monsters = [monster.Monster((8,13), [(8, 13), (8, 12), (8, 11), (7,11), (6, 11), (6, 12), (6, 13), (7,13)])]
-    ##Testcase
+
     if case == 1:
         monsters += [monster.Monster((3,1),[(3,1),(3,2),(2,2),(2,1)])]
         monsters += [monster.Monster((7,1),[(7,1),(7,2),(6,2),(6,1)])]
-        return monsters
-    elif case == 2:
-        monsters += [monster.Monster((3,1),[(3,1),(3,2),(2,2),(2,1)])]
-        monsters += [monster.Monster((7,1),[(7,1),(7,2),(6,2),(6,1)])]
         monsters += [monster.Monster((7,5),[(7,5),(7,4)])]
-        return monsters
+    elif case == 2:
+        monsters += [monster.Monster((2,10),[(2,10),(2,11),(2,12),(3,12),(3,11),(3,10)])]
+        monsters += [monster.Monster((6,4),[(6,4),(7,4),(8,4),(7,4)])]
+    elif case == 3:
+        monsters += [monster.Monster((1,3),[(1,3),(2,3)])]
+    return monsters
--- wallcreator.py
+++ wallcreator.py
@@ -1,15 +1,15 @@
 def createWalls(case):
-    #Testcase, creates walls around a 10x15
+    walls = []
+    #Barriers
+    for i in range(10):
+        walls += [(i,0)]
+        walls += [(i,14)]
+    for j in range(1,14):
+        walls += [(0,j)]
+        walls += [(9,j)]
+
+    #Cases
     if case == 1:
-        walls = []
-        for i in range(10):
-            walls += [(i,0)]
-            walls += [(i,14)]
-        for j in range(1,14):
-            walls += [(0,j)]
-            walls += [(9,j)]
-        return walls
-    elif case == 2:
         walls = []
         for i in range(10):
             walls += [(i,0)]
@@ -20,4 +20,22 @@ def createWalls(case):
         for i in range(5):
             walls += [(i,5)]
             walls += [(i+4,9)]
-        return walls
+    elif case == 2:
+        for i in range(1,10):
+            walls += [(3,i)]
+        for i in range(3,14):
+            if i != 8:
+                walls += [(5,i)]
+    elif case == 3:
+        for i in range(1,6):
+            walls += [(i,4)]
+        for i in range(2,9):
+            walls += [(i,6)]
+        for i in range(4,9):
+            for j in range(9,11):
+                walls += [(i,j)]
+        walls += [(4,11),(5,11)]
+    return walls
+        
+        
+        
