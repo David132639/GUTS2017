f45442f3590fcf0bbfbcff24be2a37c54b88dcd3
Sohva <33163737+Sohva@users.noreply.github.com>
2017-10-28 17:53:20 +0100

    merge

--- doyouhaveguts.py
+++ doyouhaveguts.py
@@ -201,49 +201,12 @@ def game():
 ##        ###FOR TESTING
 ##        otherFinished = True
 ##        ###
+        
         if otherFinished==True:
             send("xAllFinished")
 
         print "Game nearly Won!"
-##1.7 Commented out
-##        root = Tk()
-##        label = Label(root, text="Game nearly Won!")
-##        label.pack()
-##        root.mainloop()
 
-        #print "Game nearly Won!"
-
-## COMMENTED OUT 1.1 SOHVA
-##    if newloc in snake:
-##        #If we hit the snake, the game is over
-##        game_on = False
-##        for item in snake:
-##            sGrid[item[0]][item[1]].configure(bg = "red")
-##            if len(snake) - 4 > highscore:
-##                scores = open("snake_high_score.txt","w")
-##                scores.write(str(len(snake)-4))
-##                scores.close()
-##    elif newloc == food:
-##        #If we hit food, the snake won't get shorter and the location of food is changed
-##        snake += [newloc]
-##        addFood()
-##        levellabel.configure(text = "Level: " + str(len(snake)-4))
-##        if len(snake) - 4 > highscore:
-##            scorelabel.configure(text = "Record: " + str(len(snake)-4))
-##    else:
-        #The tail of the snake is removed and the new location added
-        ## SNAKE CHANGED 1.1 SOHVA
-        ## INDENTATION  CHANGED
-
-##    if newloc in snake:
-##        #If we hit the snake, the game is over
-##        game_on = False
-##        for item in snake:
-##            sGrid[item[0]][item[1]].configure(bg = "red")
-##            if len(snake) - 4 > highscore:
-##                scores = open("snake_high_score.txt","w")
-##                scores.write(str(len(snake)-4))
-##                scores.close()
     for i in range (len(foodlist)):
         if newloc == foodlist[i]:
 ##        #If we hit food, the snake won't get shorter and the location of food is changed
@@ -261,9 +224,7 @@ def game():
     
     sGrid[snake[0]][snake[1]].configure(image = grass)
     snake = newloc
-    ##LINE COMMENTED OUT 1.1 SOHVA
-            
-    ##sGrid[loc[0]][loc[1]].configure(image = snakeskin)
+
     #Make the head to point to the right direction
     nextloc = (newloc[0] + direction[0], newloc[1] + direction[1])
     if direction == (0,-1):
@@ -462,7 +423,7 @@ while True:
     if game_on:
         game()
     for i in range(10):
-        time.sleep(0.3/10)
+        time.sleep((0.35-level*0.5)/10)
         top.update_idletasks()
         top.update()
     direction = new_direction
