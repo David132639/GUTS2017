475589cc37fedec41b2d7152e439762a0391b015
Sohva <33163737+Sohva@users.noreply.github.com>
2017-10-29 10:39:01 +0000

    merge

--- doyouhaveguts.py
+++ doyouhaveguts.py
@@ -129,16 +129,19 @@ def newGame():
 
     lives = 5
     liveslabel.configure(text = "You have "+str(lives)+" attempts")
-    monsterImage = monsterImages[random.randint(0,len(monsterImages)-1)]
-
-    level = 1
-    sGrid[pumpkin[0]][pumpkin[1]].configure(image = pumpkinright)
-    createPumpkin()
-    foodlist=[]
-    #get_score()
     for row in sGrid:
         for item in row:
             item.configure(image = grass, bg = "grey")
+    monsterImage = monsterImages[random.randint(0,len(monsterImages)-1)]
+
+    level = 1
+    createWalls(level)
+    createMonsters(level)
+    createPumpkin()
+    sGrid[pumpkin[0]][pumpkin[1]].configure(image = pumpkinImage)
+    foodlist=[]
+    #get_score()
+
 
     global numFood
     numFood = random.randint(1,3)
@@ -236,9 +239,9 @@ def game():
     if newloc == finish:
         send("xOneFinished")
 
-##        ##FOR TESTING
-##        otherFinished = True
-##        ##
+        ##FOR TESTING
+        otherFinished = True
+        ##
 
         if otherFinished == True:
             send("xAllFinished")
