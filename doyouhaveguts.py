47d1c8c14a3d772d4495dcac64e4f730fc0524a3
David <David132639@users.noreply.github.com>
2017-10-29 10:07:02 +0000

    Undoing the newgame() code move

--- doyouhaveguts.py
+++ doyouhaveguts.py
@@ -20,17 +20,6 @@ pygame.mixer.init()
 pygame.mixer.music.load("res/music.mp3")
 pygame.mixer.music.play(-1)
 
-# if(top.destroy):
-# pygame.mixer.music.stop()
-
-# try:
-#    while pygame.mixer.music.get_busy() == True:
-#        continue
-# except top.destroy:
-#        pygame.mixer.music.stop()
-#       sys.exit()
-
-
 top = Tkinter.Tk()
 
 ####################### Network staff
@@ -119,6 +108,47 @@ def createPumpkin():
     global pumpkin
     ##SNAKE MODIFIED 1.1 SOHVA
     pumpkin = (1, 1)
+
+def newGame():
+    global monsterImage
+    global monsterImages
+    global pumpkin
+    global sGrid
+    global food
+    global columns
+    global rows
+    global game_on
+    global direction
+    global new_direction
+    global foodlist
+    global level
+    global lives
+
+    lives = 5
+    liveslabel.configure(text = "You have "+str(lives)+" attempts")
+    monsterImage = monsterImages[random.randint(0,len(monsterImages)-1)]
+
+    level = 1
+    sGrid[pumpkin[0]][pumpkin[1]].configure(image = pumpkinright)
+    createPumpkin()
+    foodlist=[]
+    #get_score()
+    for row in sGrid:
+        for item in row:
+            item.configure(image = grass, bg = "grey")
+
+    global numFood
+    numFood = random.randint(1,3)
+    for i in range (0, numFood):
+        addFood()
+        foodlist.append(food)
+    for food in foodlist:
+        sGrid[food[0]][food[1]].configure(image = foodimage)
+
+    game_on = True
+    direction = (0,-1)
+    new_direction = (0,-1)
+    levellabel.configure(text = "Level: " + str(level))
 
 
 def addFood():
